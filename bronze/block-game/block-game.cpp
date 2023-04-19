d298ce0a23c6af90a1b50fdc7f9aaeddb454e1b4
Ascent817 <vapradeep817@gmail.com>
2023-04-19 19:08:41 +0000

    Solve block-game

--- bronze/block-game/block-game.cpp
+++ bronze/block-game/block-game.cpp
@@ -9,8 +9,7 @@ using namespace std;
 int main()
 {
   freopen("blocks.in", "r", stdin);
-  // freopen("blocks.out", "w", stdout);
-
+  freopen("blocks.out", "w", stdout);
   int N;
   cin >> N;
 
@@ -26,16 +25,33 @@ int main()
 
   for (int i = 0; i < pow(2, N); i++)
   {
-    std::string binary = bitset<8>(i).to_string();
+    std::string binary = bitset<10>(i).to_string();
     int instance_blocks[26];
     memset(instance_blocks, 0, sizeof(instance_blocks));
 
     for (int j = 0; j < N; j++)
     {
-      int side = binary.at((8 - N) + j) - '0';
-      cout << words[j][side] << endl;
+      int side = binary.at((10 - N) + j) - '0';
+      string word = words[j][side];
+      
+      for (char c : word)
+      {
+        instance_blocks[c - (int)'a']++;
+      }
     }
-    cout << endl;
+
+    for (int k = 0; k < 26; k++)
+    {
+      if (instance_blocks[k] > blocks[k])
+      {
+        blocks[k] = instance_blocks[k];
+      }
+    }
+  }
+
+  for (int n : blocks)
+  {
+    cout << n << endl;
   }
 
   return 0;
