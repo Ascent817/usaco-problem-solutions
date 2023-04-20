abcdc9b289296708cb72b7a44e67d83833c56a96
Ascent817 <vapradeep817@gmail.com>
2023-04-20 20:54:53 +0000

    Optimize load-balancing

--- bronze/load-balancing/load-balancing.cpp
+++ bronze/load-balancing/load-balancing.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-int calculateM(int a, int b, vector<pair<int, int>> cows) {
+vector<pair<int, int>> cows;
+
+int calculateM(int a, int b) {
   vector<int> count(4);
 
   for (int i = 0; i < cows.size(); i++) {
@@ -23,30 +25,54 @@ int calculateM(int a, int b, vector<pair<int, int>> cows) {
   return max(max(count[0], count[1]), max(count[2], count[3]));
 }
 
+pair<int, int> getNext(int a, int b) {
+  int least = calculateM(a, b);
+  for (int i = a - 2; i < a + 3; i+=2) {
+    for (int j = b - 2; j < b + 3; j+=2) {
+      if (calculateM(a + i, b + j) < least) {
+        pair<int, int> pos;
+        pos.first = a + i;
+        pos.second = b + j;
+        return pos;
+      }
+    }
+  }
+  pair<int, int> pos;
+  pos.first = a + rand() % 3 - 1;
+  pos.second = b + rand() % 3 - 1;
+  return pos;
+}
+
 int main() {
   freopen("balancing.in", "r", stdin);
-  // freopen("balancing.out", "w", stdout);
-  
+  freopen("balancing.out", "w", stdout);
+
   int N, B;
   cin >> N;
   cin >> B;
-  vector<pair<int, int>> cows(N);
+
+  cows.resize(N);
 
   for (int i = 0; i < N; i++) {
     cin >> cows[i].first;
     cin >> cows[i].second;
   }
 
-  int least = 10000;
-  for (int i = 0; i < B; i+=2) {
-    for (int j = 0; j < B; j+=2) {
-      int M = calculateM(i, j, cows);
-      if (M < least) {
-        least = M;
-      }
-      cout << M << " ";
+  bool converged = false;
+  int a = (N - 1) / 2;
+  int b = (N - 1) / 2;
+  int least = calculateM(a, b);
+  vector<int> adj(4);
+
+  while (!converged) {
+    pair<int, int> next = getNext(a, b);
+    if (calculateM(next.first, next.second) < least) {
+      least = calculateM(next.first, next.second);
+      a = next.first;
+      b = next.second;
+    } else {
+      converged = true;
     }
-    cout << endl;
   }
 
   cout << least;
