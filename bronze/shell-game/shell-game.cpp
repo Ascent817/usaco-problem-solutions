7d9f00b81e97430aabde0fb72b3d9ebe10a6f42c
Ascent817 <vapradeep817@gmail.com>
2023-04-20 20:55:19 +0000

    Format old solutions

--- bronze/shell-game/shell-game.cpp
+++ bronze/shell-game/shell-game.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <cstdio>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
   freopen("shell.in", "r", stdin);
   freopen("shell.out", "w", stdout);
 
@@ -12,40 +11,31 @@ int main()
   cin >> N;
 
   int moves[N][3];
-  
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < 3; j++) {
       cin >> moves[i][j];
     }
   }
 
   int maxPoints = 0;
 
-  for (int i = 1; i < 4; i++)
-  {
+  for (int i = 1; i < 4; i++) {
     int points = 0;
-    int current = i; // 1 2 3
-    for (int j = 0; j < N; j++)
-    {
-      if (moves[j][0] == current)
-      {
+    int current = i;  // 1 2 3
+    for (int j = 0; j < N; j++) {
+      if (moves[j][0] == current) {
         current = moves[j][1];
-      }
-      else if (moves[j][1] == current)
-      {
+      } else if (moves[j][1] == current) {
         current = moves[j][0];
       }
 
-      if (moves[j][2] == current)
-      {
+      if (moves[j][2] == current) {
         points++;
       }
     }
 
-    if (points > maxPoints)
-    {
+    if (points > maxPoints) {
       maxPoints = points;
     }
   }
