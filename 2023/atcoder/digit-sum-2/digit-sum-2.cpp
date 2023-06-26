c54681fb5f94898fea6f2dd3050620adf5e8cf75
Ascent817 <vapradeep817@gmail.com>
2023-06-26 00:15:18 +0000

    Solve Digit Sum 2

--- 2023/atcoder/digit-sum-2/digit-sum-2.cpp
+++ 2023/atcoder/digit-sum-2/digit-sum-2.cpp
@@ -1,28 +1,18 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
-#define all(x) (x).begin(), (x).end()
-typedef long long ll;
 
 using namespace std;
 
 int main() {
   string N;
   cin >> N;
+  int sz = N.length();
 
-  for (int i = 1; i < N.length(); i++) {
-    char c = N[i] - '0';
-    if (c != 9) {
-      N[i - 1]--;
-      fill(N.begin() + i, N.end(), '9');
-      int sum = 0;
-
-      for (int j = 0; j < N.length(); j++) {
-        sum += N[j] - '0';
-      }
-
-      cout << sum;
+  for (int i = 1; i < sz; i++) {
+    if (N[i] != '9') {
+      cout << (N[0] - '0') + ((sz - 1) * 9) - 1;
       return 0;
     }
   }
+
+  cout << (N[0] - '0') + ((sz - 1) * 9);
 }
