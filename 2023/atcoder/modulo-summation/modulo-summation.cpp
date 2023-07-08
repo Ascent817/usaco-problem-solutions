88052f36f6227a42d5dbdc7f0bd8dc51b93eaa5b
Ascent817 <vapradeep817@gmail.com>
2023-07-08 22:56:59 +0000

    Solve Modulo Summation

--- 2023/atcoder/modulo-summation/modulo-summation.cpp
+++ 2023/atcoder/modulo-summation/modulo-summation.cpp
@@ -6,6 +6,18 @@ typedef long long ll;
 
 using namespace std;
 
+
 int main() {
-  
+  int N;
+  ll sm;
+  sm = 0;
+  cin >> N;
+
+  for (int i = 0; i < N; i++) {
+    int x;
+    cin >> x;
+    sm += x - 1;
+  }
+
+  cout << sm;
 }
