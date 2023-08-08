8b42c676e331cdb1be207729ebdd2b672116fef5
Ascent817 <vapradeep817@gmail.com>
2023-08-08 20:18:54 +0000

    Updated visualization

--- matplot/visualize.py
+++ matplot/visualize.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 
 with open('output.txt') as f:
   y = f.readlines()
 
 size = len(y)
 
-x = np.linspace(0, size, size)
-for i in range(size):
-  y[i] = int(y[i])
+x = np.arange(1, size + 1)
+y = tuple(map(int, y))
 
 fig, ax = plt.subplots()
 ax.plot(x, y)
-plt.show()
 plt.savefig('plot.png')
--- matplot/visualize.sh
+++ matplot/visualize.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 /usr/bin/g++ -fdiagnostics-color=always -g main.cpp -o main
 ./main < input.txt > output.txt
 python visualize.py
